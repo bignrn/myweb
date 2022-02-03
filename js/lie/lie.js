3baea53f28f7e1eb7107bdd8388e5e59725d71e2
Norarun <mishima.yuma3429@gmail.com>
2022-02-03 11:38:26 +0900

    json_methodeの中で条件分岐させる

--- js/lie/lie.js
+++ js/lie/lie.js
@@ -3,32 +3,33 @@ function submit() {
     const txbox = document.getElementById("search");
     const keywd = txbox.value;
 
-    const list = eroKeyWD(); //json
+    eroKeyWD(keywd); //json
     // const list = new Array("えろ", "ちんこ")
 
-    list.forEach(element => {
-        if (element == keywd) {
-            console.log("ここはエロサイトに繋がります")
-        }
-    });
+    // list.forEach(element => {
+    //     if (element == keywd) {
+    //         console.log("ここはエロサイトに繋がります")
+    //         location.href = "./BBAAAA.html"
+    //     }
+    // });
 }
 
 /**
  * エロWordのリスト作成
  */
-function eroKeyWD() {
+function eroKeyWD(key) {
     //Topicを入れる
     url = "json/ero_keywd.json";
 
     $.getJSON(url, (data) => {                                  //jsonを取得する。サーバーを経由していないと実行されない。CRF？が原因。
         const maxIdx = data.length; //マックスレングス
         var key_list = Array(maxIdx)
+
+        //検索
         for (var i = 0; i < maxIdx; i++) {
-            key_list[i] = data[i].key;   //リスト追加
-            console.log(data[i].id + " : " + data[i].key);  //jsonデータのIDごとの取得
+            if (key == data[i].key) {
+                console.log("これはエロいですね");
+            }
         }
     });
-    console.log("list=" + key_list[1]);
-
-    return key_list;
 }
