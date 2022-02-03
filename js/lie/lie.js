edeb3e500dfb7a63c0061c94fdb28716dd5fbed8
Norarun <mishima.yuma3429@gmail.com>
2022-02-03 10:42:17 +0900

    フィルタリングhtml

---
+++ js/lie/lie.js
@@ -0,0 +1,9 @@
+function submit() {
+    console.log("hello submit")
+
+    //テキスト
+    const txbox = document.getElementById("search")
+    const keywd = txbox.value
+
+    console.log(" key = " + keywd)
+}
